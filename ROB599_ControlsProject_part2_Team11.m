a3ce5be9d716eb6da8ef5465f259328d5d780a22
Haochen <haochen@Haochens-MacBook-Pro.local>
2018-12-21 11:39:37 -0500

    revised

--- ROB599_ControlsProject_part2_Team11.m
+++ ROB599_ControlsProject_part2_Team11.m
@@ -51,3 +51,55 @@ function [nearPtsIdx,angle,distError]=getNearPtsInfo(TestTrack,Pos,k)
     dist_error=cross(relativeCood,[dir 0]);
     distError=dist_error(3);% get lat distance error
 end
+function x1=ode1(U,x)
+dt=0.01;
+
+%constants
+Nw=2;
+f=0.01;
+Iz=2667;
+a=1.35;
+b=1.45;
+By=0.27;
+Cy=1.2;
+Dy=0.7;
+Ey=-1.6;
+Shy=0;
+Svy=0;
+m=1400;
+g=9.806;
+%generate input functions
+delta_f=U(1);
+F_x=U(2);
+%slip angle functions in degrees
+a_f=rad2deg(delta_f-atan2(x(4)+a*x(6),x(2)));
+a_r=rad2deg(-atan2((x(4)-b*x(6)),x(2)));
+%Nonlinear Tire Dynamics
+phi_yf=(1-Ey)*(a_f+Shy)+(Ey/By)*atan(By*(a_f+Shy));
+phi_yr=(1-Ey)*(a_r+Shy)+(Ey/By)*atan(By*(a_r+Shy));
+
+F_zf=b/(a+b)*m*g;
+F_yf=F_zf*Dy*sin(Cy*atan(By*phi_yf))+Svy;
+
+F_zr=a/(a+b)*m*g;
+F_yr=F_zr*Dy*sin(Cy*atan(By*phi_yr))+Svy;
+
+F_total=sqrt((Nw*F_x)^2+(F_yr^2));
+F_max=0.7*m*g;
+
+if F_total>F_max
+    
+    F_x=F_max/F_total*F_x;
+  
+    F_yr=F_max/F_total*F_yr;
+end
+%vehicle dynamics
+dzdt= [x(2)*cos(x(5))-x(4)*sin(x(5));...
+          (-f*m*g+Nw*F_x-F_yf*sin(delta_f))/m+x(4)*x(6);...
+          x(2)*sin(x(5))+x(4)*cos(x(5));...
+          (F_yf*cos(delta_f)+F_yr)/m-x(2)*x(6);...
+          x(6);...
+          (F_yf*a*cos(delta_f)-F_yr*b)/Iz];
+      
+x1=x+dzdt'*dt;
+end
